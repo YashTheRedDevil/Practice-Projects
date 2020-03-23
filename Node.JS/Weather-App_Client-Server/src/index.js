c7dfdb43101c68ab40745665f154a6c11b23ba51
yashthereddevil <yashthereddevil@github.com>
2020-03-23 23:36:30 +0530

    Added HBS

--- Node.JS/Weather-App_Client-Server/src/index.js
+++ Node.JS/Weather-App_Client-Server/src/index.js
@@ -1,23 +1,55 @@
 const express = require("express");
 const path = require("path");
-
-const publicDirectory = path.join(__dirname, "../public/");
+const hbs=require('hbs');
 const app = express();
 const port = 3000;
 
-//Config setup
+//Paths for express config
+const publicDirectory = path.join(__dirname, "../public/");
+const partialsPath=path.join(__dirname,'../views/partials');
+
+//Handle bars setting
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
+
+//Static directory set up
 app.use(express.static(publicDirectory));
-app.set("view engine", "hbs");
+
+
+app.get('', (req, res) => {
+  res.render('index',{
+    'title':'Weather App',
+    'name':'Yash TheRedDevil'
+  });
+});
+
+
+
+app.get('/help', (req, res) => {
+  res.render('help',{
+    'helpText':'This is Help page',
+    'name':'Yash TheRedDevil'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about',{
+    'title':'Weather App: About',
+    'name':'Yash TheRedDevil'
+  });
+});
 
 app.get("/weather", (req, res) => {
-  res.send("This is weather page");
-//   res.render('index'()=>{
-
-//   })
+  res.send({
+    'Name':'Yash',
+    'Team':'RedDevil'
+  });
 });
 
 app.get("*", (req, res) => {
-  res.send("Sorry this page does not exist");
+  res.render('404',{
+    'errorMessage':'Page Not Found'
+  });
 });
 
 app.listen(port, () => {
