9134b31aee6f0c83917a55f6d2f839d5f288557f
yashthereddevil <yashthereddevil@github.com>
2020-03-23 19:52:23 +0530

    Changes in forecast fetching

--- Node.JS/Weather-app/utils/forecast.js
+++ Node.JS/Weather-app/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = ({latitude, longitude}, callback) => {
     } else if (body.error) {
       callback("Unable to find the location", undefined);
     } else {
-      callback(undefined, body.currently);
+      callback(undefined, body.currently); 
     }
   });
 };
