99437413e9452820d5fb8540f20ad5018c375a8c
yashthereddevil <catchyogesh7@gmail.com>
2020-03-26 21:43:11 +0530

    Used async await and added method to post and get Task and User

--- Node.JS/Task-App/src/index.js
+++ Node.JS/Task-App/src/index.js
@@ -8,31 +8,133 @@ const PORT=process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users',(req,res)=>{
+app.post('/users',async(req,res)=>{
     const user=new User(req.body);
-    user.save().then((result)=>{
-        console.log(user);
-        res.send(user);
-    }).catch((error)=>{
-        console.log(error);
-        res.status(400).send(error);        
-    });    
-});
 
-app.post('/tasks',(req,res)=>{
-    const task=new Task(req.body);
-    task.save().then((result)=>{
-        res.status(201).send(task);
-    }).catch((error)=>{
+    //Using async await
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
         res.status(400).send(error);
-    })
+    }
+
+    //Without async await
+    // user.save().then((result)=>{
+    //     console.log(user);
+    //     res.status(201).send(user);
+    // }).catch((error)=>{
+    //     console.log(error);
+    //     res.status(400).send(error);        
+    // });    
+});
+
+app.get('/users',async (req,res)=>{
+    
+    //Using async await
+    try {
+        const users=await User.find({});
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+    //Without using async await
+    // User.find({}).then((users)=>{
+    //     res.status(200).send(users); 
+    // }).catch((error)=>{
+    //     res.status(500).send(error);
+    // });
+})
+
+app.get('/users/:id',async(req,res)=>{
+    const _id =req.params.id;
+
+    //With using async await
+    try {
+        const user=await User.findById(_id);
+        if(!user){
+            return res.status(404).send(user);
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+
+    //Without using async await
+    // User.findById(_id).then((user)=>{
+    //     if(!user)
+    //     {
+    //         return res.status(404).send();
+    //     }
+    //     res.status(200).send(user);
+    // }).catch((error)=>{
+    //     res.status(500).send(error);
+    // });    
 });
 
 
+app.post('/tasks',async(req,res)=>{
+    const task=new Task(req.body);
+    //With using async await
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+    //Without using async await
+    // task.save().then(()=>{
+    //     res.status(201).send(task);
+    // }).catch((error)=>{
+    //     res.status(400).send(error);
+    // })
+});
 
 
+app.get('/tasks',async(req,res)=>{
+    
+    //With using async await
+    try {
+        const tasks=await Task.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
+    //Without using async await
+    // Task.find({}).then((tasks)=>{
+    //     res.status(200).send(tasks);
+    // }).catch((error)=>{
+    //     res.status(500).send();
+    // });
+});
 
+app.get('/tasks/:id',async(req,res)=>{
+    const _id =req.params.id;
+    //With using async await 
+    try {
+        const task=await Task.findById(_id);
+        if(!task)
+        {
+            return res.status(404).send(task);
+        }
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+
+    //Without using async await 
+    // Task.findById(_id).then((tasks)=>{
+    //     if(!tasks){
+    //         return res.status(404).send();
+    //     }
+    //     res.status(200).send(tasks);
+    // }).catch((error)=>{
+    //     res.status(500).send();
+    // });
+});
 
 app.listen(PORT,()=>{
     console.log('Server is listening on '+PORT);
